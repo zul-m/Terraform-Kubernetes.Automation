d4a7851d6b586b87f2c65f64b7d3d23cd7b6cd12
Zulfikar Muhamad <zul.m@icloud.com>
2023-06-19 19:03:16 +0800

    Added cloudflare dns and api secret

---
+++ cloudflare.tf
@@ -0,0 +1,17 @@
+# Cloudflare DNS records and API secret
+resource "kubernetes_secret" "cloudflare_api_key_secret" {
+  depends_on = [
+    kubernetes_namespace.certmanager
+  ]
+
+  metadata {
+    name = ""
+    namespace = "certmanager"
+  }
+
+  data = {
+    api-key = var.cloudflare_api_key
+  }
+
+  type = "Opaque"
+}
